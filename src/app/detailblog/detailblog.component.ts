333eb2cb4f0d003137f2f164a8db3476f1d24950
Rajpal Singh <codingdrone@gmail.com>
2020-12-04 12:54:50 +0530

    seo fixes

--- src/app/detailblog/detailblog.component.ts
+++ src/app/detailblog/detailblog.component.ts
@@ -12,53 +12,51 @@ import { $ } from 'protractor';
   styleUrls: ['./detailblog.component.css']
 })
 export class DetailblogComponent implements OnInit {
-  id="";
-  title="";
-  url=``;
-  imageUrl = "";
-  blogsubDetail="<p>{{blogDetail}}</p>";
-  author="";
-  date="";
-  subimageUrl="";
-  blog_seodetail="";
-  blog_seoimageurl="";
-  blog_seokeyword="";
-  blog_seotitle="";
-  
+  id = '';
+  title = '';
+  url = ``;
+  imageUrl = '';
+  blogsubDetail = '<p>{{blogDetail}}</p>';
+  author = '';
+  date = '';
+  subimageUrl = '';
+  blog_seodetail = '';
+  blog_seoimageurl = '';
+  blog_seokeyword = '';
+  blog_seotitle = '';
 
-  constructor(private http:HttpClient, private route: ActivatedRoute,private meta: Meta, private titleService: Title) {
-    this.meta.updateTag({ name: 'description', content: this.blog_seodetail });
-    this.meta.updateTag({ name: 'author', content: this.author});
-    this.meta.updateTag({ name: 'keywords', content: this.blog_seokeyword });
-    this.meta.updateTag({ name: 'og:image', content: this.blog_seoimageurl });
-    this.setTitle(this.blog_seotitle);
-   
+  constructor(private http: HttpClient, private route: ActivatedRoute, private meta: Meta, private titleService: Title) {
   }
-   
-   // this.titleService.setTitle(`${this.seotitle }` )
-  
- 
-     
-  getDetailBlog(){
-    return this.http.get<any>(`https://dashboard.careernaksha.com/blogs/${this.id}`)
+
+  getDetailBlog() {
+    return this.http.get<any>(`https://dashboard.careernaksha.com/blogs/${this.id}`);
   }
   ngOnInit(): void {
-    
     this.url = this.route.snapshot.params.blog_url;
-    //this.url1 = this.url.toUpperCase()
-    this.id=this.route.snapshot.params.id;
+    // this.url1 = this.url.toUpperCase();
+    this.id = this.route.snapshot.params.id;
     this.getDetailBlog().subscribe(data => {
-      console.log("---data blog detail---",data);
-      this.title=data.blog_title
-      this.imageUrl=`https://dashboard.careernaksha.com${data.blog_image.url}`
-      this.blogsubDetail=data.blog_subdetail 
-      this.author=data.blog_author
-      this.date=data.blog_date
-      this.setTitle=data.blog_seotitle
-      this.subimageUrl=`https://dashboard.careernaksha.com${data.blog_subimage.url}`
+      console.log('---data blog detail---', data);
+      this.title = data.blog_title;
+      this.imageUrl = `https://dashboard.careernaksha.com${data.blog_image.url}`;
+      this.blogsubDetail = data.blog_subdetail ;
+      this.author = data.blog_author;
+      this.date = data.blog_date;
+      this.blog_seodetail = data.blog_seodetail;
+      this.subimageUrl = `https://dashboard.careernaksha.com${data.blog_image.url}`;
+
+      this.blog_seokeyword = data.blog_seokeyword;
+      this.blog_seoimageurl = data.blog_seoimageurl;
+      this.blog_seotitle = data.blog_seotitle;
+
+      this.meta.updateTag({ name: 'description', content: this.blog_seodetail });
+      this.meta.updateTag({ name: 'author', content: this.author});
+      this.meta.updateTag({ name: 'keywords', content: this.blog_seokeyword });
+      this.meta.updateTag({ name: 'og:image', content: this.blog_seoimageurl });
+
+      this.setTitle(this.blog_seotitle);
+
     });
-    
- 
   }
   public setTitle( blog_seotitle: string) {
     this.titleService.setTitle(blog_seotitle);
